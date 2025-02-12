65c00dff829b239b8431caef112e0a610719c6a9
skillnovation <skillnovation.com@gmail.com>
2025-02-12 12:08:15 -0600

    clean out the website in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProjectList } from './pages/ProjectList';
 import { ProjectDetail } from './pages/ProjectDetail';
-import { Support } from './pages/Support';
 import { Challenge } from './pages/Challenge';
 import { PWAPrompt } from './components/PWAPrompt';
 import { OfflineIndicator } from './components/OfflineIndicator';
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<ProjectList />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
-        <Route path="/support" element={<Support />} />
         <Route path="/challenge" element={<Challenge />} />
       </Routes>
       <PWAPrompt />
